e176067e5b645788e16b9fe494d0a2dad2e8bfa3
phil-kagaba <philbertiradukunda03@gmail.com>
2025-08-09 10:23:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import AdminUpload from './components/admin/AdminUpload';
+import VideoPlayer from './components/VideoPlayer';
+import Footer from './components/Footer';
+import MyMovieApis from './components/MyMovieApis';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-gray-900 min-h-screen flex flex-col text-white">
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<AdminUpload />} />
+            <Route path="/AdminUpload" element={<AdminUpload />} />
+            <Route path="/video/:filecode" element={<VideoPlayer />} />
+            <Route path="/trailer" element={<MyMovieApis />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
